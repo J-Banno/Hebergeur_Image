75857cf0f46f7259ab3258dd138deeff08ef0202
bannholtzer jonathan <bannholtzer.j@gmail.com>
2021-03-31 15:40:53 +0200

    affichage de l'image

--- index.php
+++ index.php
@@ -7,6 +7,12 @@ Script Php pour stocker une image dans un dossier.
 
 //Vérification : si image existe et qu'il n'y a pas d'erreur
 if (isset($_FILES['image']) && $_FILES['image']['error'] == 0) {
+
+
+
+    // Variable error
+    $error = 1;
+
     //Condition pour gérer la taille de l'image
     if ($_FILES['image']['size'] <= 3000000) {
         //Extension : 
@@ -19,9 +25,21 @@ if (isset($_FILES['image']) && $_FILES['image']['error'] == 0) {
 
         //On controle son extension et on renomme avec la date d'aujourd'hui et un chiffre aléatoire
         if (in_array($extensionImage, $extensionArray)) {
-            move_uploaded_file($_FILES['image']['tmp_name'], './asset' . time() . rand());
+            //Chemin de stockage 
+            $uriImage = './asset' . time() . rand() . '.' . $extensionImage;
+            move_uploaded_file($_FILES['image']['tmp_name'],  $uriImage);
+            $error = 0;
         }
     } else {
         echo "Votre Image est beaucoup trop lourde, max 3Mo.";
     }
+};
+
+
+if (isset($error) && $error == 0) {
+    echo '<div id = "containerImage">
+    <img style="max-width:100px" src="' . $uriImage . '"id ="image"/>
+    </div>' . '<a href="./index.html">
+    <button>Retour</button>
+</a>';
 }
